cf14c4c3248fda2f9425f33c5a29da157f88f70d
Ambar Mehrotra <ambar.mehrotra@capillarytech.com>
2019-07-10 16:57:52 +0530

    init tf repo

---
+++ example.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile    = "default"
+  region     = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  count = 2
+  ami           = "ami-09b68f5653871885f"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "dependent_example" {
+  depends_on = ["aws_instance.example"]
+  ami           = "ami-09b68f5653871885f"
+  instance_type = "t2.micro"
+}
