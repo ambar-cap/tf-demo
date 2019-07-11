aaa5b63f46bed438893f1b336aa33316139f5940
Ambar Mehrotra <ambar.mehrotra@capillarytech.com>
2019-07-11 14:05:40 +0530

    extra null resource in project 2

---
+++ project2/example.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  profile    = "default"
+  region     = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  count = 3
+  ami           = "ami-09b68f5653871885f"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "dependent_example" {
+  depends_on = ["aws_instance.example"]
+  ami           = "ami-09b68f5653871885f"
+  instance_type = "t2.micro"
+}
+
+resource "null_resource" "example" {}
+
+resource "null_resource" "example-2" {}
